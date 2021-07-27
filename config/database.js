db0ac75153c30e8b4a1b09393fc6add2378911d2
imanol309 <imanolguzman309@gmail.com>
2021-07-27 14:09:05 -0400

    version 2

--- config/database.js
+++ config/database.js
@@ -1,18 +1,26 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5000),
-        database: env('DATABASE_NAME', 'bank'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'luffy85pro'),
-        ssl: env.bool('DATABASE_SSL', false),
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
+          ssl: { rejectUnauthorized: false }
+        },
+        options: {
+          ssl: false
+        },
       },
-      options: {}
     },
-  },
-});
+  }
+};
